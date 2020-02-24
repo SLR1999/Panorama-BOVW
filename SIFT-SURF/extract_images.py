02b8b6cfb0d7509beec67a0aba927d2b79ae4341
SLR1999 <lalithareddy.seelam@gmail.com>
2020-02-24 18:05:55 +0530

    added ciphar code

--- SIFT-SURF/extract_images.py
+++ SIFT-SURF/extract_images.py
@@ -26,16 +26,30 @@ def saveCifarImage(array, path, file):
     # save to PNG file
     return cv2.imwrite(path+file+".png", array)
 
-batch_no = 6
-# image_array, label_array = extractImagesAndLabels("../cifar-10-batches-py/", "data_batch_"+str(batch_no))
-image_array, label_array = extractImagesAndLabels("../cifar-10-batches-py/", "test_batch")
-print(image_array.shape)
-print(label_array.shape)
+categories = extractCategories("./cifar-10-batches-py/", "batches.meta")
 
-categories = extractCategories("../cifar-10-batches-py/", "batches.meta")
-print(categories)
+for batch_no in range(5):
+    image_array, label_array = extractImagesAndLabels("./cifar-10-batches-py/", "data_batch_"+str(batch_no+1))
+    # image_array, label_array = extractImagesAndLabels("./cifar-10-batches-py/", "test_batch")
+    # print(image_array.shape)
+    # print(label_array.shape)
+
+    
+    for var in range(0,10000):
+        new_var=(batch_no)*10000+var
+        category = label_array[var].asnumpy()
+        category = (int)(category[0])
+        print(saveCifarImage(image_array[var], "./cifar-10/" + categories[category].decode("utf-8"), "/image"+str(new_var)))
+
+batch_no = 5
+# image_array, label_array = extractImagesAndLabels("./cifar-10-batches-py/", "data_batch_"+str(batch_no+1))
+image_array, label_array = extractImagesAndLabels("./cifar-10-batches-py/", "test_batch")
+# print(image_array.shape)
+# print(label_array.shape)
+
+
 for var in range(0,10000):
-    new_var=(batch_no-1)*10000+var
+    new_var=(batch_no)*10000+var
     category = label_array[var].asnumpy()
     category = (int)(category[0])
     print(saveCifarImage(image_array[var], "./cifar-10/" + categories[category].decode("utf-8"), "/image"+str(new_var)))
